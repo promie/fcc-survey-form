68358592c50e4dbd1af964f344853cd27750665c
Promie Yutasane <promie.yutasane@gmail.com>
2020-03-02 21:14:12 +1100

    Built the mobile first approach structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Survey from '@/views/Survey.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Survey
   }
 }
 </script>
 
+<template>
+  <survey />
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
 }
 </style>
+
---
+++ src/views/Survey.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper">
+    <div class="survey-header">
+      Promie Survey Form
+    </div>
+
+    <div class="survey-form">
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Survey'
+}
+</script>
+
+<style scoped>
+/* Mobile First Approach */
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+.survey-header {
+  width: 100%;
+  height: 20vh;
+  background-color: red;
+}
+
+.survey-form {
+  background-color: gray;
+  height: 80vh;
+}
+</style>
