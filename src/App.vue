5f2b3f13c9eea8692ec9b0beff8a73e05b199ca5
Promie Yutasane <promie.yutasane@gmail.com>
2020-03-03 17:17:28 +1100

    Applied background image from unsplash images site

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,13 @@ export default {
 <style>
 body {
   margin: 0;
+  background-image: url('https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3900&q=80');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-color: #464646;
 }
 </style>
+
 
